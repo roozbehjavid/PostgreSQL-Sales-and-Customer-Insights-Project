ec5e91dd5e0d4bd73889b7242ec79c9f6063fa0b
Roozbeh <91006940+roozbehjavid@users.noreply.github.com>
2024-11-23 14:48:01 +0330

    Update and rename create_index to create_index.sql

---
+++ sql/create_index.sql
@@ -0,0 +1,11 @@
+-- Index for orders_2022 table
+
+CREATE INDEX idx_order_date_2022 ON orders_2022(order_date);
+
+-- Index for orders_2023 table
+
+CREATE INDEX idx_order_date_2023 ON orders_2023(order_date);
+
+-- Index for orders_2024 table
+
+CREATE INDEX idx_order_date_2024 ON orders_2024(order_date);
